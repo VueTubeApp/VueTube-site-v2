07bfe8dba3b40cdb4e8c7ed9c1461d914720ddb9
Alex <56329333+404-Program-not-found@users.noreply.github.com>
2023-05-29 14:58:46 +1200

    feat: 💄 add Montserrat to font stack

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ['montserrat', ...defaultTheme.fontFamily.sans],
+			}
+		},
 	},
 	plugins: [],
 }
