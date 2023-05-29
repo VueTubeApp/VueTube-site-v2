821d25bcf8557af76fb386bd718390a30088147e
Alex <56329333+404-Program-not-found@users.noreply.github.com>
2023-05-29 13:18:14 +1200

    build: 📦️ add Tailwind and Vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import vue from "@astrojs/vue";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [vue(), tailwind()]
+});
