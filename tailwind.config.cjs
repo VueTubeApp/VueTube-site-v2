dd405244822664a00fe56ef1a9484c7df3256279
Nikita Krupin <krupin.nikita0110@gmail.com>
2023-05-29 03:52:16 -0400

    fix tw font import

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Montserrat", "sans-serif"],
+        sans: ["montserrat", ...defaultTheme.fontFamily.sans],
       },
     },
   },
