afc614829505465eb221e33f3a41c7d364bac7e1
Nikita Krupin <krupin.nikita0110@gmail.com>
2023-05-31 02:27:21 -0400

    mobile nav adjustments

--- src/components/Head.vue
+++ src/components/Head.vue
@@ -15,14 +15,14 @@
       id="background"
     ></div>
     <nav
-      class="z-20 order-last mt-4 w-full items-end justify-center font-semibold text-neutral-400 md:order-none md:mt-0 md:flex md:w-1/2 md:items-center"
+      class="z-20 order-last mt-4 flex w-full flex-col items-end justify-center font-semibold text-neutral-400 md:order-none md:mt-0 md:w-1/2 md:flex-row md:items-center"
       style="text-shadow: 0 0 0.5rem #0008, 0 0 0.75rem #0008"
       ref="navElement"
     >
       <!-- -mx-4 to make ensure no gaps between :hover hitboxes on scale-90 -->
       <a
         href="/"
-        class="-mx-3 block cursor-pointer whitespace-nowrap px-8 py-4 duration-150 hover:scale-90 hover:text-white"
+        class="-mr-5 block cursor-pointer whitespace-nowrap px-8 py-4 duration-150 hover:scale-90 hover:text-white md:-mx-3"
         @mouseenter="moveBackgroundToTarget"
         @mouseleave="onUnhover"
       >
@@ -30,7 +30,7 @@
       </a>
       <a
         href="/features"
-        class="-mx-3 block cursor-pointer whitespace-nowrap px-8 py-4 duration-150 hover:scale-90 hover:text-white"
+        class="-mr-5 block cursor-pointer whitespace-nowrap px-8 py-4 duration-150 hover:scale-90 hover:text-white md:-mx-3"
         @mouseenter="moveBackgroundToTarget"
         @mouseleave="onUnhover"
       >
@@ -38,21 +38,21 @@
       </a>
       <a
         href="/contribute"
-        class="-mx-3 block cursor-pointer whitespace-nowrap px-8 py-4 duration-150 hover:scale-90 hover:text-white"
+        class="-mr-5 block cursor-pointer whitespace-nowrap px-8 py-4 duration-150 hover:scale-90 hover:text-white md:-mx-3"
         @mouseenter="moveBackgroundToTarget"
         @mouseleave="onUnhover"
         >Contribute</a
       >
       <a
         href="/docs/"
-        class="-mx-3 block cursor-pointer whitespace-nowrap px-8 py-4 duration-150 hover:scale-90 hover:text-white"
+        class="-mr-5 block cursor-pointer whitespace-nowrap px-8 py-4 duration-150 hover:scale-90 hover:text-white md:-mx-3"
         @mouseenter="moveBackgroundToTarget"
         @mouseleave="onUnhover"
         >Docs</a
       >
       <a
         href="/about"
-        class="-mx-3 block cursor-pointer whitespace-nowrap px-8 py-4 duration-150 hover:scale-90 hover:text-white"
+        class="-mr-5 block cursor-pointer whitespace-nowrap px-8 py-4 duration-150 hover:scale-90 hover:text-white md:-mx-3"
         @mouseenter="moveBackgroundToTarget"
         @mouseleave="onUnhover"
         >About Us</a
