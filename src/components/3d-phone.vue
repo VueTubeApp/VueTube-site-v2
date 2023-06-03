e090b77d92ade878b94d25f29c0f381d43c00379
Nikita Krupin <krupin.nikita0110@gmail.com>
2023-06-02 23:39:14 -0400

    some working threejs code?!?!

--- src/components/3d-phone.vue
+++ src/components/3d-phone.vue
@@ -4,66 +4,45 @@
   </div>
 </template>
 <script setup>
-import * as THREE from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
-import { RGBELoader } from "three/addons/loaders/RGBELoader.js";
+// import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+// import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+// import { RGBELoader } from "three/addons/loaders/RGBELoader.js";
+import { onMounted } from "vue";
 
+import * as THREE from "three";
 onMounted(() => {
   let camera, scene, renderer;
+  let mesh;
 
   init();
-  render();
+  animate();
 
   function init() {
-    const container = document.getElementByID("canvas");
-    document.body.appendChild(container);
-
     camera = new THREE.PerspectiveCamera(
-      45,
+      70,
       window.innerWidth / window.innerHeight,
-      0.25,
-      20
+      1,
+      1000
     );
-    camera.position.set(-1.8, 0.6, 2.7);
+    camera.position.z = 400;
 
     scene = new THREE.Scene();
 
-    new RGBELoader()
-      .setPath("textures/equirectangular/")
-      .load("royal_esplanade_1k.hdr", function (texture) {
-        texture.mapping = THREE.EquirectangularReflectionMapping;
+    const texture = new THREE.TextureLoader().load("/crate.gif");
+    texture.colorSpace = THREE.SRGBColorSpace;
 
-        scene.background = texture;
-        scene.environment = texture;
+    const geometry = new THREE.BoxGeometry(200, 200, 200);
+    const material = new THREE.MeshBasicMaterial({ map: texture });
 
-        render();
-
-        // model
-
-        const loader = new GLTFLoader().setPath(
-          "models/gltf/DamagedHelmet/glTF/"
-        );
-        loader.load("DamagedHelmet.gltf", function (gltf) {
-          scene.add(gltf.scene);
-
-          render();
-        });
-      });
+    mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
 
     renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.toneMapping = THREE.ACESFilmicToneMapping;
-    renderer.toneMappingExposure = 1;
-    container.appendChild(renderer.domElement);
+    document.body.appendChild(renderer.domElement);
 
-    const controls = new OrbitControls(camera, renderer.domElement);
-    controls.addEventListener("change", render); // use if there is no animation loop
-    controls.minDistance = 2;
-    controls.maxDistance = 10;
-    controls.target.set(0, 0, -0.2);
-    controls.update();
+    //
 
     window.addEventListener("resize", onWindowResize);
   }
@@ -73,13 +52,14 @@ onMounted(() => {
     camera.updateProjectionMatrix();
 
     renderer.setSize(window.innerWidth, window.innerHeight);
-
-    render();
   }
 
-  //
+  function animate() {
+    requestAnimationFrame(animate);
 
-  function render() {
+    mesh.rotation.x += 0.005;
+    mesh.rotation.y += 0.01;
+
     renderer.render(scene, camera);
   }
 });
