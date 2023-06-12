d02a6bdc16022430d2dead697a43a3c0cd513c11
Alex <hello@missingprogram.me>
2023-06-12 14:49:34 +1200

    feat(home): :sparkles: contrib graphs are now links

--- src/components/Contributors.vue
+++ src/components/Contributors.vue
@@ -1,20 +1,27 @@
 <template>
-  <div
+  <a
     v-for="contributor in contributors"
     class="h-16 w-16 rounded-full border border-neutral-600 duration-300 hover:border-neutral-400"
     style="box-shadow: 0 0 0.5rem -0.1rem #fff8"
+    :href="contributor.html_url"
+    :aria-label="contributor.login"
     :style="{
-      backgroundImage: `url('${contributor}')`,
+      backgroundImage: `url('${contributor.avatar_url}')`,
       backgroundSize: 'cover',
       backgroundPosition: 'center',
     }"
-  ></div>
+  ></a>
 </template>
 <script lang="ts" setup>
-const props = defineProps({
-  contributors: {
-    type: Array,
-    required: true,
-  },
-});
+interface ContributorType {
+  avatar_url: string;
+  html_url: string;
+  login: string;
+}
+
+interface Props {
+  contributors: ContributorType[];
+}
+
+defineProps<Props>();
 </script>
